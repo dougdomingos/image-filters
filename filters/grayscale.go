03cab465446594e950939a4e7f0cc3951e363dee
Douglas Domingos <doug.domingossilva@gmail.com>
2025-04-19 13:43:06 -0300

    feat(filters): add implementation of Grayscale filter

---
+++ filters/grayscale.go
@@ -0,0 +1,39 @@
+package filters
+
+import (
+	"image"
+	"image/color"
+
+	"dougdomingos.com/image-filters/utils"
+)
+
+// Grayscale converts each pixel within a specified segment of the image into
+// its corresponding shade of gray.
+//
+// This implementation uses the Rec. 601 luma transform to compute the
+// grayscale value. The alpha channel is left unmodified.
+func Grayscale(img *image.RGBA, bounds image.Rectangle) {
+	for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
+		for x := bounds.Min.X; x < bounds.Max.X; x++ {
+			r, g, b, a := utils.GetRGBA8(img, x, y)
+			gray := runLumaTransform(r, g, b)
+			img.Set(x, y, color.RGBA{gray, gray, gray, a})
+		}
+	}
+}
+
+// runLumaTransform computes the grayscale value of a pixel based on the Rec. 601
+// luma transform:
+//
+//  gray = (0.299 * R) + (0.587 * G) + (0.114 * B)
+//
+// The Rec. 601 standard accounts for human visual perception, assigning higher
+// weight to the green channel,followed by red and blue, based on the eye's
+// sensitivity to each color.
+func runLumaTransform(r, g, b uint8) uint8 {
+	weightedRed   := 0.299 * float64(r)
+	weightedGreen := 0.587 * float64(g)
+	weightedBlue  := 0.114 * float64(b)
+
+	return uint8(weightedRed + weightedGreen + weightedBlue)
+}

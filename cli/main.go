6df7938f1af8a8355e2f67d33694728a28bc43ee
Douglas Domingos <doug.domingossilva@gmail.com>
2025-04-19 00:43:34 -0300

    feat(cli): add flags for execution mode and workers

--- cli/main.go
+++ cli/main.go
@@ -1,14 +1,28 @@
 package main
 
 import (
-	"fmt"
 	"flag"
+	"fmt"
+	"os"
 )
 
 func main() {
 	inputImage := flag.String("img", "", "Path to image file")
 	filterName := flag.String("filter", "", "Filter to apply")
+	executionMode := flag.String("mode", "serial", "Execution mode: serial or concurrent")
+	workers := flag.Int("workers", 1, "Number of threads used to process the image in concurrent mode")
 	flag.Parse()
+
+	if *executionMode == "serial" {
+		// this shall work, eventually...
+		fmt.Printf("Running on %s mode...\n", *executionMode)
+	} else if *executionMode == "concurrent" {
+		// and so will this.
+		fmt.Printf("Running on %s mode with %d worker(s)...\n", *executionMode, *workers)
+	} else {
+		fmt.Printf("[ERROR] Unknown execution mode: %s\n", *executionMode)
+		os.Exit(1)
+	}
 
 	fmt.Printf("Image file: \"%s\", filter \"%s\"\n", *inputImage, *filterName)
 }

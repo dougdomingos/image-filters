6c6da6fad091bdaca233cb14a46c8b2c1ce43163
Douglas Domingos <doug.domingossilva@gmail.com>
2025-04-23 23:36:28 -0300

    feat(utils): return output path from SaveImage function

--- utils/persistence.go
+++ utils/persistence.go
@@ -34,24 +34,24 @@ func LoadImage(filePath string) (*image.RGBA, string, error) {
 // SaveImage takes a RGBA image, its original encoding format and the path in
 // which it'll be stored and creates a new file containing the specified image
 // at that path.
-func SaveImage(img *image.RGBA, format string, outputDir string, outputFile string) error {
-	file, err := os.Create(filepath.Join(outputDir, outputFile))
+func SaveImage(img *image.RGBA, format string, outputDir string, outputFile string) (string, error) {
+	outputPath := filepath.Join(outputDir, outputFile)
+	file, err := os.Create(outputPath)
 	if err != nil {
-		return fmt.Errorf("[ERROR] Unable to create file \"%s\": %w", outputDir, err)
+		return "", fmt.Errorf("[ERROR] Unable to create file \"%s\": %w", outputDir, err)
 	}
 	defer file.Close()
 
-	var encodingErr error
 	switch format {
 	case "jpeg":
-		encodingErr = jpeg.Encode(file, img, &jpeg.Options{Quality: 95})
+		err = jpeg.Encode(file, img, &jpeg.Options{Quality: 95})
 	case "png":
-		encodingErr = png.Encode(file, img)
+		err = png.Encode(file, img)
 	default:
-		encodingErr = fmt.Errorf("[ERROR] Unsuported format: %s", format)
+		return "", fmt.Errorf("[ERROR] Unsupported image format")
 	}
 
-	return encodingErr
+	return outputPath, err
 }
 
 // CreateOutputDir checks if a directory exists at the given path, and creates

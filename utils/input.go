e69d0acd94d68a1f4ee94cdd08bb58323023f331
Douglas Domingos <doug.domingossilva@gmail.com>
2025-04-23 22:38:50 -0300

    feat(utils): add flag to provide output directory for images

--- utils/input.go
+++ utils/input.go
@@ -5,13 +5,14 @@ import (
 	"fmt"
 )
 
-func ParseInputFlags() (string, string, string) {
+func ParseInputFlags() (string, string, string, string) {
 	imgPath := flag.String("img", "", "Path to image file")
+	outputDir := flag.String("outputDir", "./output", "Directory where the processed image shall be stored")
 	filterName := flag.String("filter", "", "Filter to apply")
 	executionMode := flag.String("mode", "serial", "Execution mode: serial or concurrent")
 	flag.Parse()
 
-	return *imgPath, *filterName, *executionMode
+	return *imgPath, *outputDir, *filterName, *executionMode
 }
 
 func ValidateExecutionMode(mode string) error {

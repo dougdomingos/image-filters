fb9422494cc462c782a98c925eaef851fb4df767
Douglas Domingos <doug.domingossilva@gmail.com>
2025-04-23 23:35:51 -0300

    feat(filters): add filter map for user selection

--- filters/types.go
+++ filters/types.go
@@ -2,7 +2,10 @@
 // pixel-based manipulation techniques.
 package filters
 
-import "image"
+import (
+	"fmt"
+	"image"
+)
 
 // Filter is a function that applies a certain processing technique within a
 // defined segment of a image.
@@ -15,3 +18,23 @@ import "image"
 // the image that the filter should operate on. The filter directly alters
 // `img`.
 type Filter func(image *image.RGBA, bounds image.Rectangle)
+
+// AvailableFilters maps a string identifier to its corresponding image filter
+// function.
+var AvaliableFilters = map[string]Filter{
+	"grayscale": Grayscale,
+	"binarization": Binarization,
+	// add more filters here...
+}
+
+// GetFilter retrieves a filter function by its name from the AvailableFilters
+// map. It returns the filter if found, or an error if the filter name is not
+// defined.
+func GetFilter(filterName string) (Filter, error) {
+	filter, exists := AvaliableFilters[filterName]
+	if !exists {
+		return nil, fmt.Errorf("[ERROR]: Specified filter does not exist")
+	}
+
+	return filter, nil
+} 

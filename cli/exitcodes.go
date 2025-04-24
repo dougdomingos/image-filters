98f1ff4f158d053315fcfd9e9374e2b2e9dcf53d
Douglas Domingos <doug.domingossilva@gmail.com>
2025-04-23 23:37:01 -0300

    feat(cli): add constants for error exitcodes

---
+++ cli/exitcodes.go
@@ -0,0 +1,9 @@
+package main
+
+const (
+	ImageLoadingError         = 2
+	ImageSavingError          = 3
+	FilterNotFoundError       = 4
+	InvalidExecutionModeError = 5
+	OutputDirError            = 6
+)

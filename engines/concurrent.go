fc014b4616d6f9e9da7fd8112bceaadb5246e9fa
Douglas Domingos <doug.domingossilva@gmail.com>
2025-04-26 22:44:19 -0300

    refactor: remove filter application logic from cli endpoint script

--- engines/concurrent.go
+++ engines/concurrent.go
@@ -11,18 +11,22 @@ import (
 
 // ExecuteConcurrent applies the provided filter to the specified image object
 // by splitting it into several segments, assigning each one to a goroutine.
-func ExecuteConcurrent(img *image.RGBA, filter filters.Filter) {
+func ExecuteConcurrent(img *image.RGBA, pipeline filters.FilterPipeline) {
 	var (
 		workers     = runtime.NumCPU()
 		imageStrips = utils.GetVerticalStrips(img.Bounds(), workers)
 		wg sync.WaitGroup
 	)
+
+	if pipeline.Preprocess != nil {
+		ExecuteConcurrent(img, *pipeline.Preprocess)
+	}
 	
 	wg.Add(len(imageStrips))
 	for strip := range imageStrips {
 		go func(s image.Rectangle) {
 			defer wg.Done()
-			filter(img, s)
+			pipeline.Filter(img, s)
 		}(imageStrips[strip])
 	}
 
--- engines/serial.go
+++ engines/serial.go
@@ -8,6 +8,10 @@ import (
 // ExecuteSerial applies the provided filter to the specified image object
 // as a whole, without any sort of partition. As such, all processing is
 // done with a single thread.
-func ExecuteSerial(image *image.RGBA, filter filters.Filter) {
-	filter(image, image.Bounds())
+func ExecuteSerial(image *image.RGBA, pipeline filters.FilterPipeline) {
+	if pipeline.Preprocess != nil {
+		ExecuteSerial(image, *pipeline.Preprocess)
+	}
+
+	pipeline.Filter(image, image.Bounds())
 }

9c15290fa08cb06d407bd47e01446dd1d5b70ccd
Douglas Domingos <doug.domingossilva@gmail.com>
2025-05-01 17:25:39 -0300

    feat(utils): add function to copy image partitions with padding

--- utils/image.go
+++ utils/image.go
@@ -24,7 +24,6 @@ func SwapPixels(img *image.RGBA, offsetX, offsetY int) {
 	}
 }
 
-
 // CopyImagePartition creates a copy of a region of the provided image.
 func CopyImagePartition(img *image.RGBA, bounds image.Rectangle) image.RGBA {
 	copiedPartition := image.NewRGBA(bounds)
@@ -39,4 +38,50 @@ func CopyImagePartition(img *image.RGBA, bounds image.Rectangle) image.RGBA {
 	}
 
 	return *copiedPartition
+}
+
+func CopyImagePartitionWithPadding(img *image.RGBA, bounds image.Rectangle, padding int) image.RGBA {
+	paddedBounds := GetPaddedBounds(bounds, padding)
+	paddedImg := image.NewRGBA(paddedBounds)
+
+	for y := paddedBounds.Min.Y; y < paddedBounds.Max.Y; y++ {
+		copyRowStart := y * paddedImg.Stride
+		for x := paddedBounds.Min.X; x < paddedBounds.Max.X; x++ {
+			copyOffset := copyRowStart + (x-paddedImg.Rect.Min.X)*4
+
+			if !IsPositionWithinOriginalImage(img, x, y, padding) {
+				copy(paddedImg.Pix[copyOffset:copyOffset+4], []uint8{0, 0, 0, 255})
+			} else {
+				srcRowStart := (y - padding) * img.Stride
+				srcOffset := srcRowStart + (x-padding-img.Rect.Min.X)*4
+				copy(paddedImg.Pix[copyOffset:copyOffset+4], img.Pix[srcOffset:srcOffset+4])
+			}
+		}
+	}
+
+	return *paddedImg
+}
+
+func IsPositionWithinOriginalImage(img *image.RGBA, x, y, padding int) bool {
+	offsetX, offsetY := x-padding, y-padding
+
+	isXWithinBounds := offsetX >= img.Rect.Min.X && offsetX < img.Rect.Max.X
+	isYWithinBounds := offsetY >= img.Rect.Min.Y && offsetY < img.Rect.Max.Y
+
+	return isXWithinBounds && isYWithinBounds
+}
+
+func GetPaddedBounds(bounds image.Rectangle, padding int) image.Rectangle {
+	minX, maxX := bounds.Min.X-padding, bounds.Max.X+padding
+	minY, maxY := bounds.Min.Y-padding, bounds.Max.Y+padding
+
+	if bounds.Min.X == 0 {
+		minX, maxX = 0, bounds.Max.X+(2*padding)
+	}
+
+	if bounds.Min.Y == 0 {
+		minY, maxY = 0, bounds.Max.Y+(2*padding)
+	}
+
+	return image.Rect(minX, minY, maxX, maxY)
 }

88d8a251f8520f2c5046bbe1bb38174a14c78caa
Douglas Domingos <doug.domingossilva@gmail.com>
2025-07-20 22:41:57 -0300

    refactor: add CLI entrypoint

---
+++ cmd/cli/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "dougdomingos.com/image-filters/internals/cli"
+
+func main() {
+	flags := cli.ParseInputFlags()
+
+	if flags.ListPipelines {
+		cli.ListAvaliablePipelines()
+	} else {
+		cli.ApplyPipelineToImage(flags.ImgPath, flags.OutputDir, flags.FilterName, flags.IsConcurrent)
+	}
+}

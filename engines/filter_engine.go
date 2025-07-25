95f4eaf5dc3b40f183f71244d66b56b768f94d1a
Douglas Domingos <doug.domingossilva@gmail.com>
2025-07-24 23:30:37 -0300

    refactor: rename engine function to reflect changes on app structure

--- engines/filter_engine.go
+++ engines/filter_engine.go
@@ -6,9 +6,7 @@ import (
 	"dougdomingos.com/image-filters/pipelines"
 )
 
-// ApplyFilterPipeline handles the execution of a filter pipeline, applying the
-// preprocess filter (if present) and then the core filter implementation.
-func ApplyFilterPipeline(img *image.RGBA, recipe *pipelines.Recipe) {
+func ProcessRecipe(img *image.RGBA, recipe *pipelines.Recipe) {
 	currentStep := recipe.NextStep()
 	for currentStep != nil {
 		if currentStep.Action.Preprocess != nil {

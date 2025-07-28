38fdecae1e30b2a7de2e967c009822fffac9a99d
Douglas Domingos <doug.domingossilva@gmail.com>
2025-07-28 20:24:55 -0300

    refactor(api): move port and env var validation to api internals

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,27 +1,11 @@
 package main
 
 import (
-	"log"
-	"os"
-	"strconv"
-
 	"dougdomingos.com/image-filters/internals/api"
-	"dougdomingos.com/image-filters/internals/utils"
 	"github.com/joho/godotenv"
 )
 
 func main() {
 	godotenv.Load()
-
-	requiredVars := []string{"API_OUTPUT_DIR", "MAX_REQUEST_SIZE"}
-	if err := utils.EnsureRequiredEnvVars(requiredVars); err != nil {
-		log.Fatal(err)
-	}
-
-	serverPort, err := strconv.Atoi(os.Getenv("API_SERVER_PORT"))
-	if err != nil {
-		serverPort = 8080
-	}
-
-	api.StartHTTPServer(serverPort)
+	api.StartHTTPServer()
 }
--- internals/api/server.go
+++ internals/api/server.go
@@ -4,16 +4,35 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"strconv"
+
+	"dougdomingos.com/image-filters/internals/utils"
 )
 
 // StartHTTPServer initiates the API HTTP server with the specified routes.
 // By default, the server listens to port 8080.
-func StartHTTPServer(port int) {
+func StartHTTPServer() {
 	router := withRequestLogger(buildRouter())
+	port := getServerPort()
 	address := fmt.Sprintf(":%d", port)
+
+	requiredVars := []string{"API_OUTPUT_DIR", "MAX_REQUEST_SIZE"}
+	if err := utils.EnsureRequiredEnvVars(requiredVars); err != nil {
+		log.Fatal(err)
+	}
 
 	log.Printf("Starting HTTP server on port %d\n", port)
 	if err := http.ListenAndServe(address, router); err != nil {
 		log.Fatalf("Could not start HTTP server: %s\n", err.Error())
 	}
+}
+
+func getServerPort() int {
+	serverPort, err := strconv.Atoi(os.Getenv("API_SERVER_PORT"))
+	if err != nil {
+		serverPort = 8080
+	}
+
+	return serverPort
 }

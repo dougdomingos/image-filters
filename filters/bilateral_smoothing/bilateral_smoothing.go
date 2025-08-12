cf320432133b2100cce3e3ba24c8f805a4c0699b
Douglas Domingos <doug.domingossilva@gmail.com>
2025-08-12 16:22:40 -0300

    feat(filters): add base files for bilateral-smoothing implementation

---
+++ filters/bilateral_smoothing/bilateral_smoothing.go
@@ -0,0 +1,17 @@
+package bilateralsmoothing
+
+import (
+	"image"
+
+	"dougdomingos.com/image-filters/filters/types"
+)
+
+var BilateralSmoothingFilter = types.NewFilter(nil, BilateralSmoothing)
+
+func BilateralSmoothing(img *image.RGBA)  {
+	// TODO
+}
+
+func bilateralSmoothingWorker() {
+	// TODO
+}
